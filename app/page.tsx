e0661a663567b1cffae96f0ac6c75862db2117db
nguyenplayground <shiroenguyenlehoang@gmail.com>
2025-09-09 09:40:03 +0700

    Write my first short story

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        lhnguyen
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a story teller and a world builder. 
+          I tell my story through motion pictures, sketch and words.`}
       </p>
       <div className="my-8">
         <BlogPosts />
